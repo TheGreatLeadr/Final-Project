f446341d41dc02fa259758d03bc31fbd91958e73
gr8passm8 <codetreknxtgen@gmail.com>
2017-05-16 00:31:59 +0200

    This is a new screenshot

--- gogogo.sh
+++
@@ -1,2 +0,0 @@
-git commit -a -m "This is a new screenshot"
-git push --all
---
+++ testscript.sh
@@ -0,0 +1,3 @@
+git add --all || echo "Failed to add files to local branch!"
+git commit -a -m "This is a new screenshot"
+git push --all
