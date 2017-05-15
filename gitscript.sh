d86c3ceabdfde3b1a7fe1a00371c3db2ae3c72f6
gr8passm8 <codetreknxtgen@gmail.com>
2017-05-16 00:30:18 +0200

    This is a new screenshot

--- gitscript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd F:\Uni\2. Semester\Final Github\Final-Project
-git add --all || echo "Failed to add files to local branch!"
-git push --m "Final-Project" || echo "Failed to commit changes!"
-git push --all || echo "Failed to push to master"
